41064ed2340cbf956a841c6929f8263ad5e3b5af
Alumno Practica Desarrollo 1 <alumno.desarrollo1@CRECIC.CL>
2016-07-20 14:19:26 -0400

    Primer commit

---
+++ app/src/main/java/com/example/alumnodesarrollo1/protototipo_punto_de_venta/VentaActivity.java
@@ -0,0 +1,13 @@
+package com.example.alumnodesarrollo1.protototipo_punto_de_venta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VentaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_venta);
+    }
+}
---
+++ app/src/test/java/com/example/alumnodesarrollo1/protototipo_punto_de_venta/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.example.alumnodesarrollo1.protototipo_punto_de_venta;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
